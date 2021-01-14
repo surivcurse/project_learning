67019e6c05d4558e7d462982a2593e0220504cbd
Akawit Panutai <akawit.panutai@hotmail.com>
2021-01-14 13:25:39 +0700

    spit template file to header , footer

---
+++ app/Views/templates/footer.php
@@ -0,0 +1,98 @@
+<!-- footer area container -->
+<div class="footer-area bg-dark text-gray">
+    <!-- aside -->
+    <aside class="aside container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 col-md-3 col">
+                <div class="logo"><a href="<?= base_url() ?>"><img src="<?= base_url('images/logo.png') ?>" alt="studyLMS"></a></div>
+                <p>We have over 20 years experience providing expert Educational both businesses and individuals. Our Investment Committee brings cades the industry expertise in driving our investment approach. portfolio constructor and allocation</p>
+                <a href="#" class="btn btn-default text-uppercase">Start Leaning Now</a>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-3 col hidden-xs">
+                <h3>Popular Courses</h3>
+                <!-- widget cources list -->
+                <ul class="widget-cources-list list-unstyled">
+                    <li>
+                        <a href="course-single.html">
+                            <div class="alignleft">
+                                <img src="http://placehold.it/60x60" alt="image description">
+                            </div>
+                            <div class="description-wrap">
+                                <h4>Introduction to Mobile Apps Development</h4>
+                                <strong class="price text-primary element-block font-lato text-uppercase">$99.00</strong>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="course-single.html">
+                            <div class="alignleft">
+                                <img src="http://placehold.it/60x60" alt="image description">
+                            </div>
+                            <div class="description-wrap">
+                                <h4>Become a Professional Film Maker</h4>
+                                <strong class="price text-success element-block font-lato text-uppercase">Free</strong>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="course-single.html">
+                            <div class="alignleft">
+                                <img src="http://placehold.it/60x60" alt="image description">
+                            </div>
+                            <div class="description-wrap">
+                                <h4>Swift Programming For Beginners</h4>
+                                <strong class="price text-primary element-block font-lato text-uppercase">$75.00</strong>
+                            </div>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <nav class="col-xs-12 col-sm-6 col-md-3 col">
+                <h3>Quick Links</h3>
+                <!-- fooer navigation -->
+                <ul class="fooer-navigation list-unstyled">
+                    <li><a href="#">All Courses</a></li>
+                    <li><a href="#">Summer Sessions</a></li>
+                    <li><a href="#">Recent Exams</a></li>
+                    <li><a href="#">Professional Courses</a></li>
+                    <li><a href="#">Privacy Policy</a></li>
+                    <li><a href="#">Terms of Use</a></li>
+                    <li><a href="#">All Courses</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+            </nav>
+            <div class="col-xs-12 col-sm-6 col-md-3 col">
+                <h3>contact us</h3>
+                <p>If you want to contact us about any issue our support available to help you 8am-7pm Monday to saturday.</p>
+                <!-- ft address -->
+                <address class="ft-address">
+                    <dl>
+                        <dt><span class="fas fa-map-marker"><span class="sr-only">marker</span></span></dt>
+                        <dd>Address: 9015 Sunset Boulevard United Kingdom</dd>
+                        <dt><span class="fas fa-phone-square"><span class="sr-only">phone</span></span></dt>
+                        <dd>Call: <a href="tel:+2156237532">+ 215 623 7532</a></dd>
+                        <dt><span class="fas fa-envelope-square"><span class="sr-only">email</span></span></dt>
+                        <dd>Email: <a href="mailto:info@Studylms.com">info@Studylms.com</a></dd>
+                    </dl>
+                </address>
+            </div>
+        </div>
+    </aside>
+<!-- page footer -->
+<footer id="page-footer" class="font-lato">
+    <div class="container">
+        <div class="row holder">
+            <div class="col-xs-12 col-sm-push-6 col-sm-6">
+                <ul class="socail-networks list-unstyled">
+                    <li><a href="#"><span class="fab fa-facebook"></span></a></li>
+                    <li><a href="#"><span class="fab fa-twitter"></span></a></li>
+                    <li><a href="#"><span class="fab fa-instagram"></span></a></li>
+                    <li><a href="#"><span class="fab fa-linkedin"></span></a></li>
+                </ul>
+            </div>
+            <div class="col-xs-12 col-sm-pull-6 col-sm-6">
+                <p><a href="#">Studylms</a> | &copy; 2018 <a href="#">DesignFalls</a>, All rights reserved</p>
+            </div>
+        </div>
+    </div>
+</footer>
