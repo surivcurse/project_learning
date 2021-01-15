78d2b4ff54869849016ac7aecf393366f1ca83f1
Pichchan <pichchan@bunditcenter.com>
2021-01-15 20:20:40 +0700

    update member register

--- app/Models/MemberModel.php
+++ app/Models/MemberModel.php
@@ -27,8 +27,8 @@ class MemberModel extends Model
 
     public function __construct(ConnectionInterface &$db  = null )
     {
-        $this->setValidationRules($validationRules);
-        $this->setValidationMessages($validationMessages);   
+        $this->setValidationRules($this->validationRules);
+        $this->setValidationMessages($this->validationMessages);   
         if ($db instanceof ConnectionInterface)
         {
             $this->db =& $db;
