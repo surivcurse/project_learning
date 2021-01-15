2545741e764de9bd87976e5b91a472ac51fc0d83
Pichchan <pichchan@bunditcenter.com>
2021-01-15 22:30:18 +0700

    update member register

--- app/Controllers/Api/Member.php
+++ app/Controllers/Api/Member.php
@@ -17,12 +17,21 @@ class Member extends BaseController
       
         $member_data = [];
 
+        // $member_id  = (int)$_SERVER['HTTP_MID'];
+        // $token = $_SERVER['HTTP_TOKEN'];
+
         foreach ($this->memberModel->getFields() as $keyfield){
             if(isset($_POST[$keyfield])){
                 $member_data[$key]  = $_POST[$keyfield];
             }
         }
 
+        if( $this->memberModel->add($member_data) === false){
+            $response['errors'] = $this->memberModel->errors();
+
+            return $this->setResponseFormat('json')->failValidationError($response);  
+        }
+       
 
         // $email = $_POST['email'];
         // $email = $_POST['email'];
